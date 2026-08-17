98c02e6c7bc73455b2122e5029f9b3a355faf784
Shreya-Bhat-2006 <iamshreyabhat@gmail.com>
2026-08-17 17:08:22 +0530

    Organize string rotation solution

---
+++ Strings/String_Rotation/code.py
@@ -0,0 +1,11 @@
+def is_rotation(s1, s2):
+    if len(s1) != len(s2):
+        return False
+
+    return s2 in s1 + s1
+
+
+s1 = input("Enter first string: ")
+s2 = input("Enter second string: ")
+
+print(is_rotation(s1, s2))
